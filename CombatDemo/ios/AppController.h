e57df99612064202d057f04eab1d25566d25bb0c
hachen <hachen@zynga.com>
2013-10-04 20:50:11 +0800

    init

---
+++ CombatDemo/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  CombatDemoAppController.h
+//  CombatDemo
+//
+//  Created by Hao Chen on 10/4/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ CombatDemo/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  CombatDemoAppController.h
+//  CombatDemo
+//
+//  Created by Hao Chen on 10/4/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
